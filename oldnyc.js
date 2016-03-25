a6dd2fb56e383acc8860869930e270d70b95be9d
Bert Spaan <bert.spaan@gmail.com>
2016-03-25 17:42:38 -0400

    new data-module structure, standardjs, json-ld context

--- oldnyc.js
+++ oldnyc.js
@@ -1,83 +1,64 @@
-var fs = require('fs');
-var path = require('path');
-var request = require('request');
-var H = require('highland');
-var JSONStream = require('JSONStream');
+var fs = require('fs')
+var path = require('path')
+var request = require('request')
+var H = require('highland')
+var JSONStream = require('JSONStream')
 
-var baseUrl = 'https://www.oldnyc.org/';
-var counts = 'lat-lon-counts.js';
+var baseUrl = 'https://www.oldnyc.org/'
+var counts = 'lat-lon-counts.js'
 
-var downloadLatlonFile = function(latLon, callback) {
-  var url = baseUrl + 'by-location/' + latLon.replace(',', '') + '.json';
-  console.log('\tdownloading ' + latLon);
+var downloadLatlonFile = function (latLon, callback) {
+  var url = baseUrl + 'by-location/' + latLon.replace(',', '') + '.json'
+  console.log('\tdownloading ' + latLon)
 
   request(url, {json: true}, function (err, response, json) {
     if (err) {
-      callback(err);
+      callback(err)
+      return
     }
+
     callback(null, {
-      coordinates: latLon.split(',').reverse().map(function(c) {
-        return parseFloat(c);
-      }),
+      coordinates: latLon.split(',').reverse().map((c) => parseFloat(c)),
       data: json
-    });
-  });
-};
+    })
+  })
+}
 
-var writePit = function(writer, pit, callback) {
-  var data = [
-    {
-      type: 'pit',
-      obj: pit
-    }
-  ];
-
-  writer.writeObjects(data, function(err) {
-    callback(err);
-  });
-};
-
-function download(config, dir, writer, callback) {
+function download (config, dirs, tools, callback) {
   H(request(baseUrl + counts))
     .splitBy(':')
-    .map(function(line) {
-      return line.match(/(-?\d+\.\d*)/g);
+    .map(function (line) {
+      return line.match(/(-?\d+\.\d*)/g)
     })
     .compact()
-    .toArray(function(latLons) {
+    .toArray((latLons) => {
       H(latLons)
-        .map(function(latLon) {
-          return latLon.join(',');
-        })
+        .map((latLon) => latLon.join(','))
         .map(H.curry(downloadLatlonFile))
         .nfcall([])
         .series()
-        .errors(function(err) {
-          console.error(err);
-        })
+        .errors(callback)
         .pipe(JSONStream.stringify())
-        .on('end', function() {
-          callback();
-        })
-        .pipe(fs.createWriteStream(path.join(dir, 'data.json')));
-    });
+        .on('end', callback)
+        .pipe(fs.createWriteStream(path.join(dirs.current, 'data.json')))
+    })
 }
 
-function convert(config, dir, writer, callback) {
-  var stream = fs.createReadStream(path.join(dir, 'data.json'))
-    .pipe(JSONStream.parse('*'));
+function transform (config, dirs, tools, callback) {
+  var stream = fs.createReadStream(path.join(dirs.previous, 'data.json'))
+    .pipe(JSONStream.parse('*'))
 
   H(stream)
-    .map(function(d) {
+    .map(function (d) {
       var geometry = {
         type: 'Point',
         coordinates: d.coordinates
-      };
+      }
 
-      return Object.keys(d.data).map(function(key) {
-        var obj = d.data[key];
+      return Object.keys(d.data).map(function (key) {
+        var obj = d.data[key]
 
-        var imageId = key.split('-')[0];
+        var imageId = key.split('-')[0]
 
         var pit = {
           id: key,
@@ -88,45 +69,45 @@ function convert(config, dir, writer, callback) {
             nyplUrl: 'http://digitalcollections.nypl.org/items/image_id/' + imageId
           },
           geometry: geometry
-        };
+        }
 
         if (obj.original_title) {
-          pit.name = obj.original_title;
+          pit.name = obj.original_title
         }
 
         if (obj.date) {
-          var matches = obj.date.match(/(\b\d{4}\b)/g);
+          var matches = obj.date.match(/(\b\d{4}\b)/g)
           if (matches) {
-            var years = matches.map(function(m) {
-                return parseInt(m);
-              }).sort();
+            var years = matches.map(function (m) {
+              return parseInt(m)
+            }).sort()
 
-            var minYear = years[0];
-            var maxYear = years[years.length - 1];
+            var minYear = years[0]
+            var maxYear = years[years.length - 1]
 
-            pit.validSince = minYear;
-            pit.validUntil = maxYear;
+            pit.validSince = minYear
+            pit.validUntil = maxYear
           }
         }
 
-        return pit;
-      });
+        return pit
+      })
     })
     .flatten()
-    .map(H.curry(writePit, writer))
+    .map((pit) => ({
+      type: 'pit',
+      obj: pit
+    }))
+    .map(H.curry(tools.writer.writeObject))
     .nfcall([])
     .series()
-    .errors(function(err){
-      console.error(err);
-    })
-    .done(function() {
-      callback();
-    });
+    .errors(callback)
+    .done(callback)
 }
 
 // ==================================== API ====================================
 
 module.exports.steps = [
   download,
-  convert
-];
+  transform
+]
